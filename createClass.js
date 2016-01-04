0c18ea0d03af929c2aeffd52a436f4aee6f44ee2
wanghongxin492 <wanghongxin492@emao.com>
2016-01-04 16:26:54 +0800

    add closure and prototype shadow

--- createClass.js
+++ createClass.js
@@ -1,78 +1,119 @@
+;void(function(factory, root) {
+    root._f = factory.call(this);
+}(function() {
+    var f = {};
+    f.createClass = createClass;
+    f.inherit = inherit;
+    f.mixin = mixin;
 
-var f={};
-f.createClass=createClass;
-f.inherit=inherit;
-f.mixin=mixin;
+    function plain() {}
 
-function parent(prototype) { //父类的实例存放实例初始化和class初始化属性
-    for (var i in prototype) {
-        if (prototype.hasOwnProperty(i)) {
-            this[i] = prototype[i];
+    function createClass(prototype, initIns, initClass, farther) {
+        plain.prototype = farther ? farther.prototype : undefined;
+        var type = function() {
+            if (!this instanceof type) {
+                return new type(arguments);
+            }
+            if (initIns) {
+                initIns.apply(this, arguments);
+            }
+            if (farther) {
+                farther.apply(this, arguments);
+            }
+            this.initialize && this.initialize.apply(this, arguments);
+        };
+
+        if (initClass) {
+            type.__initClass__ = initClass;
+            initClass(type);
+        }
+        if (initIns) type.__initIns__ = initIns;
+
+
+
+        type.extend = extend;
+
+        var proto = farther ? new plain : {};
+        mixin(proto, prototype);
+        proto.constructor = type;
+        type.prototype = proto;
+        return type;
+    }
+
+    function inherit(type, seed, initIns, initClass) {
+        initIns = initIns || plain;
+        initClass = initClass || plain;
+        var child = function() {
+            if (!this instanceof child) {
+                console.log(new child(arguments))
+                return new child(arguments);
+            }
+            type.apply(this, arguments);
+            initIns && initIns.apply(this, arguments);
+        };
+        initClass(child);
+        plain.prototype = type.prototype;
+
+        var proto = new plain;
+        child.prototype = proto;
+        proto.constructor = child;
+        mixin(proto, seed);
+        return child;
+    }
+
+    function mixin(des, src) {
+        for (var i in src) {
+            if (src.hasOwnProperty(i)) {
+                des[i] = src[i];
+            }
         }
     }
-};
-function plain() {};
-function createClass(prototype, __initIns, __initClass) {
 
-    __initIns = __initIns || plain;
-    __initClass = __initClass || plain;
-    var parentProto = {
-        __initIns__: __initIns,
-        __initClass__: __initClass
-    };
-    parent.prototype = parentProto;
-    var f = function() {
-        this.__initIns__.apply(this, arguments);
-        this.initialize && this.initialize.apply(this,arguments);
-    };
-
-    f.__initClass__ = __initClass;
-    __initClass(f);
-    f.__initIns__ = __initIns;
-    f.extend=extend;
-
-    var fProto = new parent(prototype);
-    fProto.constructor = f;
-    f.prototype = fProto;
+    function extend(seed, initIns, initClass) {
+        return inherit(this, seed || {}, initIns, initClass);
+    }
     return f;
-};
-function inherit(seed,another,initialize){
-    var child= function(){
-        another.apply(this,arguments);
-        initialize&&initialize.apply(this,arguments);
-    };
-    plain.prototype=another.prototype;
+}, this));
 
-    var proto=new plain;
-    plain.prototype=proto;
-    proto=new plain;
-    child.prototype=proto;
-    mixin(proto,seed);
-    return child;
-}
+var Widget = _f.createClass({
+    type: 'Widget',
+    initialize: function(name) {
+        console.log([name, 'widget initialized!'].join(' '));
+    }
+}, function(name) {
+    this.name = name;
+    this._name = name;
+}, function(Widget) {
+    Widget.type = 'Widget';
+});
 
-function mixin(des,src){
-    for(var i in src){
-        if(src.hasOwnProperty(i)){
-            des[i]=src[i];
+var Lover = Widget.extend({
+    type: 'Lover',
+    initialize: function(name) {
+        console.log([name, 'lover initialized']);
+    }
+}, function(name) {
+    this.name = name;
+}, function(Lover) {
+    Lover.type = 'Lover';
+});
+
+
+var M = _f.createClass({
+        type: 'M',
+        initialize: function(name) {
+            this.self = name;
         }
-    }
-}
-
-function extend(seed,initialize){
-    return inherit(seed,this,initialize);
-}
-
-var View=createClass({
-    a:1,
-    getA:function(){
-        return this.a;
-    }
-},function(x){
-    this.b=x;
-},function(_class){
-    _class.alert=alert;
-});
-var View2=View.extend({c:3},function(x){
-    this.d=x;
-});
+    },
+    function(name) {
+        this.M = name
+    },
+    function(M) {
+        M.self = 'M';
+    },
+    Widget)
+var n=_f.inherit(M,{n:'n'},function(){
+    alert(3);
+},function(n){
+    n.alert=alert;
+})
